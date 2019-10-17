a7d8107b5572de1199315dfaa97d92074dc8a888
unknown <873619879@qq.com>
2019-10-18 00:14:02 +0800

    nodeblog

---
+++ app.js
@@ -0,0 +1,43 @@
+const http = require('http')
+const querystring = require('querystring')
+const {
+    hostName,
+    port
+} = require('./src/config/config.js')
+
+const app = http.createServer((req, res) => {
+    // 处理get请求的数据
+    const method = req.method
+    const url = req.url
+    console.log(req.method)
+    const path = url.split('?')[0]
+    const query = querystring.parse(url.split('?')[1])
+    res.setHeader('Content-type', 'application/json')
+    const resData = {
+        method,
+        url,
+        path,
+        query
+    }
+    if (req.method == 'GET') {
+        res.end(JSON.stringify(resData))
+    }
+    // 处理post请求的数据
+    if (req.method == 'POST') {
+        let postData = ''
+        req.on('data', (data) => {
+            postData += data.toString()
+        })
+        req.on('end', () => {
+            resData.postData = postData
+            console.log(postData)
+            res.end(JSON.stringify(resData))
+        })
+    }
+    // 处理静态页面返回
+    res.end()
+})
+
+app.listen(port, hostName, () => {
+    console.log(`server is runing success at http://${hostName}:${port}`)
+})
---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    hostName: '127.0.0.1',
+    port: 8088
+}
